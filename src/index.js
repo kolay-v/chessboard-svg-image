b7049e464a731f6b83c2a25ac53d102c01784d83
Piterden <efremov.a.denis@gmail.com>
2021-02-10 17:29:20 +0300

    Added customisation

--- src/index.js
+++ src/index.js
@@ -2,56 +2,50 @@
 
 const fs = require('fs')
 const express = require('express')
-const { fabric } = require('fabric')
+const { StaticCanvas, loadSVGFromString, util: { groupSVGElements } } = require('fabric').fabric
 const { Board } = require('chess/dist/board')
 
 const app = express()
-const defaultSvg = fs.readFileSync('board.svg', { encoding: 'utf-8' })
+const stubSvg = fs.readFileSync('stub.svg', { encoding: 'utf-8' })
 
+const W_CELL_COLOR = 'f0d9b5'
+const B_CELL_COLOR = 'b58863'
+const BG_COLOR = '212121'
+const TEXT_COLOR = 'e5e5e5'
+const MARKS_COLOR = 'aaa23b'
+const MARKS_SIZE = 7
+const BOARD_SIZE = 512
 const SQUARE_SIZE = 45
 const BOARD_PADDING = 15
 const FILES = 'abcdefgh'
-const COORDINATES = {
-  1: '<path d="M6.754 26.996h2.578v-8.898l-2.805.562v-1.437l2.79-.563h1.578v10.336h2.578v1.328h-6.72z"/>',
-  2: '<path d="M8.195 26.996h5.508v1.328H6.297v-1.328q.898-.93 2.445-2.492 1.555-1.57 1.953-2.024.758-.851 1.055-1.437.305-.594.305-1.164 0-.93-.657-1.516-.648-.586-1.695-.586-.742 0-1.57.258-.82.258-1.758.781v-1.593q.953-.383 1.781-.578.828-.196 1.516-.196 1.812 0 2.89.906 1.079.907 1.079 2.422 0 .72-.274 1.368-.265.64-.976 1.515-.196.227-1.243 1.313-1.046 1.078-2.953 3.023z"/>',
-  3: '<path d="M11.434 22.035q1.132.242 1.765 1.008.64.766.64 1.89 0 1.727-1.187 2.672-1.187.946-3.375.946-.734 0-1.515-.149-.774-.14-1.602-.43V26.45q.656.383 1.438.578.78.196 1.632.196 1.485 0 2.258-.586.782-.586.782-1.703 0-1.032-.727-1.61-.719-.586-2.008-.586h-1.36v-1.297h1.423q1.164 0 1.78-.46.618-.47.618-1.344 0-.899-.64-1.375-.633-.485-1.82-.485-.65 0-1.391.141-.743.14-1.633.437V16.95q.898-.25 1.68-.375.788-.125 1.484-.125 1.797 0 2.844.82 1.046.813 1.046 2.204 0 .968-.554 1.64-.555.664-1.578.922z"/>',
-  4: '<path d="M11.016 18.035L7.03 24.262h3.985zm-.414-1.375h1.984v7.602h1.664v1.312h-1.664v2.75h-1.57v-2.75H5.75v-1.523z"/>',
-  5: '<path d="M6.719 16.66h6.195v1.328h-4.75v2.86q.344-.118.688-.172.343-.063.687-.063 1.953 0 3.094 1.07 1.14 1.07 1.14 2.899 0 1.883-1.171 2.93-1.172 1.039-3.305 1.039-.735 0-1.5-.125-.758-.125-1.57-.375v-1.586q.703.383 1.453.57.75.188 1.586.188 1.351 0 2.14-.711.79-.711.79-1.93 0-1.219-.79-1.93-.789-.71-2.14-.71-.633 0-1.266.14-.625.14-1.281.438z"/>',
-  6: '<path d="M10.137 21.863q-1.063 0-1.688.727-.617.726-.617 1.992 0 1.258.617 1.992.625.727 1.688.727 1.062 0 1.68-.727.624-.734.624-1.992 0-1.266-.625-1.992-.617-.727-1.68-.727zm3.133-4.945v1.437q-.594-.28-1.204-.43-.601-.148-1.195-.148-1.562 0-2.39 1.055-.82 1.055-.938 3.188.46-.68 1.156-1.04.696-.367 1.531-.367 1.758 0 2.774 1.07 1.023 1.063 1.023 2.899 0 1.797-1.062 2.883-1.063 1.086-2.828 1.086-2.024 0-3.094-1.547-1.07-1.555-1.07-4.5 0-2.766 1.312-4.406 1.313-1.649 3.524-1.649.593 0 1.195.117.61.118 1.266.352z"/>',
-  7: '<path d="M6.25 16.66h7.5v.672L9.516 28.324H7.867l3.985-10.336H6.25z"/>',
-  8: '<path d="M10 22.785q-1.125 0-1.773.602-.641.601-.641 1.656t.64 1.656q.649.602 1.774.602t1.773-.602q.649-.61.649-1.656 0-1.055-.649-1.656-.64-.602-1.773-.602zm-1.578-.672q-1.016-.25-1.586-.945-.563-.695-.563-1.695 0-1.399.993-2.211 1-.813 2.734-.813 1.742 0 2.734.813.993.812.993 2.21 0 1-.57 1.696-.563.695-1.571.945 1.14.266 1.773 1.04.641.773.641 1.89 0 1.695-1.04 2.602-1.03.906-2.96.906t-2.969-.906Q6 26.738 6 25.043q0-1.117.64-1.89.641-.774 1.782-1.04zm-.578-2.492q0 .906.562 1.414.57.508 1.594.508 1.016 0 1.586-.508.578-.508.578-1.414 0-.906-.578-1.414-.57-.508-1.586-.508-1.023 0-1.594.508-.562.508-.562 1.414z"/>',
-  a: '<path d="M23.328 10.016q-1.742 0-2.414.398-.672.398-.672 1.36 0 .765.5 1.218.508.445 1.375.445 1.196 0 1.914-.843.727-.852.727-2.258v-.32zm2.867-.594v4.992h-1.437v-1.328q-.492.797-1.227 1.18-.734.375-1.797.375-1.343 0-2.14-.75-.79-.758-.79-2.024 0-1.476.985-2.226.992-.75 2.953-.75h2.016V8.75q0-.992-.656-1.531-.649-.547-1.829-.547-.75 0-1.46.18-.711.18-1.368.539V6.062q.79-.304 1.532-.453.742-.156 1.445-.156 1.898 0 2.836.984.937.985.937 2.985z"/>',
-  b: '<path d="M24.922 10.047q0-1.586-.656-2.485-.649-.906-1.79-.906-1.14 0-1.796.906-.649.899-.649 2.485 0 1.586.649 2.492.656.898 1.797.898 1.14 0 1.789-.898.656-.906.656-2.492zm-4.89-3.055q.452-.781 1.14-1.156.695-.383 1.656-.383 1.594 0 2.586 1.266 1 1.265 1 3.328 0 2.062-1 3.328-.992 1.266-2.586 1.266-.96 0-1.656-.375-.688-.383-1.14-1.164v1.312h-1.446V2.258h1.445z"/>',
-  c: '<path d="M25.96 6v1.344q-.608-.336-1.226-.5-.609-.172-1.234-.172-1.398 0-2.172.89-.773.883-.773 2.485 0 1.601.773 2.492.774.883 2.172.883.625 0 1.234-.164.618-.172 1.227-.508v1.328q-.602.281-1.25.422-.64.14-1.367.14-1.977 0-3.14-1.242-1.165-1.242-1.165-3.351 0-2.14 1.172-3.367 1.18-1.227 3.227-1.227.664 0 1.296.14.633.134 1.227.407z"/>',
-  d: '<path d="M24.973 6.992V2.258h1.437v12.156h-1.437v-1.312q-.453.78-1.149 1.164-.687.375-1.656.375-1.586 0-2.586-1.266-.992-1.266-.992-3.328 0-2.063.992-3.328 1-1.266 2.586-1.266.969 0 1.656.383.696.375 1.149 1.156zm-4.899 3.055q0 1.586.649 2.492.656.898 1.797.898 1.14 0 1.796-.898.657-.906.657-2.492 0-1.586-.657-2.485-.656-.906-1.796-.906-1.141 0-1.797.906-.649.899-.649 2.485z"/>',
-  e: '<path d="M26.555 9.68v.703h-6.61q.094 1.484.89 2.265.806.774 2.235.774.828 0 1.602-.203.781-.203 1.547-.61v1.36q-.774.328-1.586.5-.813.172-1.649.172-2.093 0-3.32-1.22-1.219-1.218-1.219-3.296 0-2.148 1.157-3.406 1.164-1.266 3.132-1.266 1.766 0 2.79 1.14 1.03 1.134 1.03 3.087zm-1.438-.422q-.015-1.18-.664-1.883-.64-.703-1.703-.703-1.203 0-1.93.68-.718.68-.828 1.914z"/>',
-  f: '<path d="M25.285 2.258v1.195H23.91q-.773 0-1.078.313-.297.312-.297 1.125v.773h2.367v1.117h-2.367v7.633H21.09V6.781h-1.375V5.664h1.375v-.61q0-1.46.68-2.124.68-.672 2.156-.672z"/>',
-  g: '<path d="M24.973 9.937q0-1.562-.649-2.421-.64-.86-1.804-.86-1.157 0-1.805.86-.64.859-.64 2.421 0 1.555.64 2.415.648.859 1.805.859 1.164 0 1.804-.86.649-.859.649-2.414zm1.437 3.391q0 2.234-.992 3.32-.992 1.094-3.04 1.094-.757 0-1.429-.117-.672-.11-1.304-.344v-1.398q.632.344 1.25.508.617.164 1.257.164 1.414 0 2.118-.743.703-.734.703-2.226v-.711q-.446.773-1.141 1.156-.695.383-1.664.383-1.61 0-2.594-1.227-.984-1.226-.984-3.25 0-2.03.984-3.257.985-1.227 2.594-1.227.969 0 1.664.383t1.14 1.156V5.664h1.438z"/>',
-  h: '<path d="M26.164 9.133v5.281h-1.437V9.18q0-1.243-.485-1.86-.484-.617-1.453-.617-1.164 0-1.836.742-.672.742-.672 2.024v4.945h-1.445V2.258h1.445v4.765q.516-.789 1.211-1.18.703-.39 1.617-.39 1.508 0 2.282.938.773.93.773 2.742z"/>',
-}
 
 const renderSVG = (board, {
-  whiteBottom = true,
   marks = [],
-  marksColor = 'aaa23b',
-  marksSize = '7',
+  bgColor,
+  marksSize,
+  textColor,
+  marksColor,
+  bCellColor,
+  wCellColor,
+  squareSize,
+  whiteBottom,
 }) => {
   const svgElements = []
 
   for (let i = 0; i < board.squares.length; i += 1) {
     const { file, rank, piece } = board.squares[i]
     const fileNumber = FILES.indexOf(file) + 1
-    const x = ((whiteBottom ? 9 - fileNumber : fileNumber) - 1) * SQUARE_SIZE + BOARD_PADDING
-    const y = ((whiteBottom ? rank : 9 - rank) - 1) * SQUARE_SIZE + BOARD_PADDING
-    const color = (fileNumber + rank) % 2 ? '#f0d9b5' : '#b58863'
+    const x = ((whiteBottom ? 9 - fileNumber : fileNumber) - 1) * squareSize + BOARD_PADDING
+    const y = ((whiteBottom ? rank : 9 - rank) - 1) * squareSize + BOARD_PADDING
+    const color = (fileNumber + rank) % 2 ? wCellColor : bCellColor
     const squareId = `${file}${rank}`
 
-    svgElements.push(`<rect x="${x}" y="${y}" width="${SQUARE_SIZE}" height="${SQUARE_SIZE}" class="square ${squareId}" stroke="none" fill="${color}"/>`)
+    svgElements.push(`<rect x="${x}" y="${y}" width="${squareSize}" height="${squareSize}" class="square ${squareId}" stroke="none" fill="#${color}"/>`)
 
     if (piece) {
       svgElements.push(`<use xlink:href="#${piece.side.name}-${piece.type}" transform="translate(${x}, ${y})"/>`)
     } else if (marks.includes(squareId)) {
-      svgElements.push(`<circle cx="${x + SQUARE_SIZE / 2}" cy="${y + SQUARE_SIZE / 2}" r="${marksSize}" fill="#${marksColor}"/>`)
+      svgElements.push(`<circle cx="${x + squareSize / 2}" cy="${y + squareSize / 2}" r="${marksSize}" fill="#${marksColor}"/>`)
     }
   }
 
@@ -62,14 +56,14 @@ const renderSVG = (board, {
     const file = horizontal[whiteBottom ? 8 - i - 1 : i]
     const rank = vertical[whiteBottom ? 8 - i - 1 : i]
 
-    svgElements.push(`<g transform="translate(${20 + i * SQUARE_SIZE}, 0) scale(0.75 0.75)" fill="#e5e5e5" stroke="#e5e5e5">${COORDINATES[file]}</g>`)
-    svgElements.push(`<g transform="translate(${20 + i * SQUARE_SIZE}, ${SQUARE_SIZE * 8 + BOARD_PADDING}) scale(0.75 0.75)" fill="#e5e5e5" stroke="#e5e5e5">${COORDINATES[file]}</g>`)
+    svgElements.push(`<text transform="translate(${BOARD_PADDING + squareSize / 2 + i * squareSize - 3}, 10) scale(.65)" fill="#${textColor}">${file.toUpperCase()}</text>`)
+    svgElements.push(`<text transform="translate(${squareSize + i * squareSize - 10}, ${squareSize * 8 + BOARD_PADDING * 2 - 3}) scale(.65)" fill="#${textColor}">${file.toUpperCase()}</text>`)
 
-    svgElements.push(`<g transform="translate(0, ${20 + i * SQUARE_SIZE}) scale(0.75 0.75)" fill="#e5e5e5" stroke="#e5e5e5">${COORDINATES[rank]}</g>`)
-    svgElements.push(`<g transform="translate(${SQUARE_SIZE * 8 + BOARD_PADDING}, ${20 + i * SQUARE_SIZE}) scale(0.75 0.75)" fill="#e5e5e5" stroke="#e5e5e5">${COORDINATES[rank]}</g>`)
+    svgElements.push(`<text transform="translate(4, ${squareSize + i * squareSize - 3}) scale(.7)" fill="#${textColor}">${rank}</text>`)
+    svgElements.push(`<text transform="translate(${squareSize * 8 + BOARD_PADDING * 2 - 10}, ${squareSize + i * squareSize - 3}) scale(.7)" fill="#${textColor}">${rank}</text>`)
   }
 
-  return defaultSvg.split('{{board}}').join(svgElements.join(''))
+  return stubSvg.split('{{bg}}').join(bgColor).split('{{board}}').join(svgElements.join(''))
 }
 
 app.get('/:fen.jpeg', (req, res) => {
@@ -77,32 +71,49 @@ app.get('/:fen.jpeg', (req, res) => {
   const {
     rotate = 0,
     marks: marksList = '',
-    marks_color: marksColor,
-    marks_size: marksSize,
+    bg_color: bgColor = BG_COLOR,
+    marks_color: marksColor = MARKS_COLOR,
+    marks_size: marksSize = MARKS_SIZE,
+    board_size: boardSize = BOARD_SIZE,
+    text_color: textColor = TEXT_COLOR,
+    square_size: squareSize = SQUARE_SIZE,
+    board_padding: boardPadding = BOARD_PADDING,
+    w_cell_color: wCellColor = W_CELL_COLOR,
+    b_cell_color: bCellColor = B_CELL_COLOR,
   } = req.query
   const { fen = 'rnbkqbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBKQBNR' } = req.params
 
   const marks = marksList.split(',')
-
-  const size = 1024
   const whiteBottom = !!Number(rotate)
 
   res.contentType('image/jpeg')
 
-  const svg = renderSVG(Board.load(fen), { whiteBottom, marks, marksColor, marksSize })
-  const canvas = new fabric.Canvas()
+  const svg = renderSVG(Board.load(fen), {
+    marks,
+    bgColor,
+    marksSize,
+    textColor,
+    marksColor,
+    squareSize,
+    bCellColor,
+    wCellColor,
+    whiteBottom,
+    boardPadding,
+  })
 
-  canvas.setHeight(size)
-  canvas.setWidth(size)
+  const canvas = new StaticCanvas('c', {
+    width: boardSize,
+    height: boardSize,
+  })
 
-  fabric.loadSVGFromString(svg, (objects, info) => {
+  loadSVGFromString(svg, (objects, info) => {
     const ctx = canvas.getContext('2d')
-    const scaleX = info.width ? (size / info.width) : 1
-    const scaleY = info.height ? (size / info.height) : 1
+    const scaleX = info.width ? (boardSize / info.width) : 1
+    const scaleY = info.height ? (boardSize / info.height) : 1
 
     ctx.scale(scaleX, scaleY)
 
-    const obj = fabric.util.groupSVGElements(objects, info)
+    const obj = groupSVGElements(objects, info)
 
     canvas.add(obj)
     canvas.renderAll()
